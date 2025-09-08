4635cfb7dd8a42e8cc8dd22b28c88bb39443a3ef
AbullahMazharIqbal1 <abdullahmazhar4578@gmail.com>
2025-09-08 15:04:24 +0500

    Fix GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/ProposalAI/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
